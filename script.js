9f5416ba5859cdf8371f13fc27281034e07b35e7
lmwale24 <124707787+lmwale24@users.noreply.github.com>
2024-02-01 18:05:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 100);
+});
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
